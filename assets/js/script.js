3bcff8393b18b1bfe391cac3ecf2315db62b3190
NiketGirdhar22 <niket.girdhar2022@vitstudent.ac.in>
2024-11-17 03:56:21 +0530

    css changes - responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 function scrollToAbout() {
     document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
 }
+
+// JavaScript to toggle the navbar
+document.getElementById('hamburger').addEventListener('click', function() {
+    // Toggle the 'open' class for both hamburger and nav-links
+    document.getElementById('nav-links').classList.toggle('open');
+    this.classList.toggle('open');
+});
